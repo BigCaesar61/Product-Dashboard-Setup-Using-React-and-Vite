871480beea4ba84464b141d7ea4c800c5487ce42
Martin Leone <martinnikolaslll@gmail.com>
2024-11-17 15:31:38 -0500

    Create AddProductForm component with form fields

---
+++ product-dashboard/src/AddProductForm.jsx
@@ -0,0 +1,25 @@
+import React, {useState} from "react";
+
+function AddProductForm() {
+    const [product, setProducts] = useState('');
+
+    const handleChange = (event) => {
+        setProducts(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert(`Product Submitted: ${product}`);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Poduct:
+                <input type="product" value={product} onChange={handleChange} />
+            </label>
+        </form>
+    );
+}
+
+export default AddProductForm
